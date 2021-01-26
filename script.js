14ecf483131ef6ded6f4abf6840b9e3cac4512c6
Suhaila <suben@microsoft.com>
2021-01-25 23:34:00 -0800

    can change second player now

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const gameBoard = (() => {
-        let data = [];
-        for (let i = 0; i < 9 ; i++) {
-            data.push(0);
-        }
+    let data = [];
+    for (let i = 0; i < 9 ; i++) {
+        data.push(0);
+    }
     const playerMaker = (name, image) => {
     return { name, image };
     };
@@ -29,16 +29,19 @@ function render() {
             targ.setAttribute('class', gameBoard.playerOne.image);
         } else if (gameBoard.data[i] === 2) {
             let targ = document.querySelector(`[data-index="${i}"`);
-            targ.setAttribute('class', gameBoard.playerTwo.image);
+            targ.setAttribute('class', Menu.opponent.image);
         }
     }
+}
+
+function updateState() {
     const A = gameBoard.data
     let announcement = document.querySelector('h2');
     if ((A[0] === 1 && A[1] === 1 && A[2] === 1) || (A[3] === 1 && A[4] === 1 && A[5] === 1) || (A[6] === 1 && A[7] === 1 && A[8] === 1) || (A[0] === 1 && A[3] === 1 && A[6] === 1) || (A[1] === 1 && A[4] === 1 && A[7] === 1) || (A[2] === 1 && A[5] === 1 && A[8] === 1) || (A[0] === 1 && A[4] === 1 && A[8] === 1) || (A[2] === 1 && A[4] === 1 && A[6] === 1)) {
         announcement.textContent = `${gameBoard.playerOne.name} wins!`;
         gameBoard.won = true;
     } else if ((A[0] === 2 && A[1] === 2 && A[2] === 2) || (A[3] === 2 && A[4] === 2 && A[5] === 2) || (A[6] === 2 && A[7] === 2 && A[8] === 2) || (A[0] === 2 && A[3] === 2 && A[6] === 2) || (A[1] === 2 && A[4] === 2 && A[7] === 2) || (A[2] === 2 && A[5] === 2 && A[8] === 2) || (A[0] === 2 && A[4] === 2 && A[8] === 2) || (A[2] === 2 && A[4] === 2 && A[6] === 2)) {
-        announcement.textContent = `${gameBoard.playerTwo.name} wins!`;
+        announcement.textContent = `${Menu.opponent.name} wins!`;
         gameBoard.won = true;
     } else if (!A.includes(0)) {
         announcement.textContent = 'A tie!';
@@ -53,9 +56,24 @@ function play(element) {
         } else {
             gameBoard.data[move] = 2
         }
-        render();
-        gameBoard.turn ? gameBoard.turn = false : gameBoard.turn = true;
+    gameBoard.turn ? gameBoard.turn = false : gameBoard.turn = true;
+    if (Menu.opponent === gameBoard.computerPlayer || Menu.opponent === gameBoard.Totoro) {
+        computerPlay();
     }
+        render();
+        updateState();
+        
+    }
+}
+
+function computerPlay() {
+    for (i = 0; i < 9 ; i++) {
+        if (gameBoard.data[i] === 0) {
+            gameBoard.data[i] = 2;
+            break;
+        }
+    }
+    gameBoard.turn = true;
 }
 
 const Menu = (() => {
@@ -72,9 +90,7 @@ const Menu = (() => {
         }
     }
     theButton.addEventListener('click', toggleMenu)
-    let resetButton = document.querySelector('#reset');
-    resetButton.addEventListener('click', reset)
-    function reset() {
+   const reset = () => {
         gameBoard.data = [];
         for (let i = 0; i < 9 ; i++) {
             gameBoard.data.push(0);
@@ -82,12 +98,27 @@ const Menu = (() => {
         gameBoard.won = false;
         gameBoard.turn = true;
         render();
+        updateState();
     }
+    let resetButton = document.querySelector('#reset');
+    resetButton.addEventListener('click', reset)
     let nameOne = document.querySelector('#nameone')
     nameOne.addEventListener('click', () => rename('playerOne'))
     let nameTwo = document.querySelector('#nametwo')
     nameTwo.addEventListener('click', () => rename('playerTwo'))
     function rename(whomever) {
-        gameBoard[whomever]['name'] = prompt('What name would you like?', 'X')
+        gameBoard[whomever]['name'] = prompt(`What is ${gameBoard[whomever]['name']}'s name?`, 'X')
     }
+    let opponent = gameBoard.playerTwo;
+    function changeOpponent(select) {
+        Menu.opponent = select;
+        reset();
+    }
+    let humanButton = document.querySelector('#human');
+    let aiButton = document.querySelector('#ai');
+    let totoroButton = document.querySelector('#Totoro');
+    humanButton.addEventListener('click', () => changeOpponent(gameBoard.playerTwo));
+    aiButton.addEventListener('click', () => changeOpponent(gameBoard.computerPlayer));
+    totoroButton.addEventListener('click', () => changeOpponent(gameBoard.Totoro));
+    return { opponent, reset }
 })()
